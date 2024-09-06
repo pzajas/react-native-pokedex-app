5087d9f249ba0cbfe6c017ba5a439b64a771e204
pzajas <zajas.piotr@gmail.com>
2024-09-07 01:40:07 +0200

    Initial commit.

---
+++ app/_layout.tsx
@@ -0,0 +1,50 @@
+import { useColorScheme } from '@/components/useColorScheme'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import { useFonts } from 'expo-font'
+import { Stack } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import 'react-native-reanimated'
+import SpaceMono from '../assets/fonts/SpaceMono-Regular.ttf'
+
+export const unstable_settings = {
+  initialRouteName: '(tabs)'
+}
+
+SplashScreen.preventAutoHideAsync()
+
+export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    SpaceMono,
+    ...FontAwesome.font
+  })
+
+  useEffect(() => {
+    if (error) throw error
+  }, [error])
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return <RootLayoutNav />
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme()
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ThemeProvider>
+  )
+}
