a50e1b2f7b96e13fd5733cd5c81215dfd2bf3f64
pzajas <zajas.piotr@gmail.com>
2024-10-02 23:44:31 +0200

    Add initial media tab gallery list with add photo button.

--- screens/pokemon/tabs/media/Media.tsx
+++ screens/pokemon/tabs/media/Media.tsx
@@ -1,35 +1,137 @@
+import { SmallRoundButton } from '@/components/buttons/SmallRoundButton'
 import { useNameLocalSearchParams } from '@/hooks/useNameLocalSearchParams'
+import { storage } from '@/services/firebase/firebase'
 import { usePokemonData } from '@/services/hooks/usePokemonData'
-import { ScrollView, StyleSheet } from 'react-native'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import * as ImageManipulator from 'expo-image-manipulator'
+import * as ImagePicker from 'expo-image-picker'
+import { getDownloadURL, listAll, ref, uploadBytesResumable } from 'firebase/storage'
+import { Alert, Image, ScrollView, StyleSheet, View } from 'react-native'
+import { PokeTabSectionHeader } from '../PokeTabSectionHeader'
 import { PokemonCries } from './components/PokemonCries'
+
+const fetchImages = async (name) => {
+  const storageRef = ref(storage, `images/${name}/`)
+  const listResponse = await listAll(storageRef)
+  const urls = await Promise.all(listResponse.items.map((item) => getDownloadURL(item)))
+  return urls
+}
+
+const uploadImage = async (pickedUri, name) => {
+  const response = await fetch(pickedUri)
+  const blob = await response.blob()
+  const storageRef = ref(storage, `images/${name}/${Date.now()}`)
+  const uploadTask = uploadBytesResumable(storageRef, blob)
+  return new Promise((resolve, reject) => {
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {},
+      (error) => {
+        console.error('Upload failed:', error)
+        reject(error)
+      },
+      async () => {
+        const url = await getDownloadURL(uploadTask.snapshot.ref)
+        resolve(url)
+      }
+    )
+  })
+}
 
 export const Media = () => {
   const { name } = useNameLocalSearchParams()
   const { cries } = usePokemonData(name)
+  const queryClient = useQueryClient()
+
+  const { data: imageUris = [] } = useQuery({
+    queryKey: ['images', name],
+    queryFn: () => fetchImages(name),
+    enabled: !!name
+  })
+
+  const uploadImageMutation = useMutation({
+    mutationFn: (pickedUri) => uploadImage(pickedUri, name),
+    onSuccess: (url) => {
+      Alert.alert('Upload successful', 'Image has been uploaded successfully!')
+    },
+    onError: (error) => {
+      console.error('Error uploading image:', error)
+      Alert.alert('Upload failed', error.message)
+    }
+  })
+
+  const pickAndUploadImage = async () => {
+    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync()
+    if (!permissionResult.granted) {
+      Alert.alert('Permission to access the camera roll is required!')
+      return
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      quality: 1
+    })
+
+    if (!result.canceled) {
+      const pickedUri = result.assets[0].uri
+      console.log('Picked Image URI:', pickedUri)
+
+      const manipResult = await ImageManipulator.manipulateAsync(pickedUri, [{ resize: { width: 800 } }], {
+        compress: 0.7,
+        format: ImageManipulator.SaveFormat.WEBP
+      })
+
+      const compressedUri = manipResult.uri
+      console.log('Compressed Image URI (WEBP):', compressedUri)
+
+      queryClient.setQueryData(['images', name], (oldData) => [...oldData, compressedUri])
+      uploadImageMutation.mutate(compressedUri)
+    }
+  }
 
   return (
-    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
-      {<PokemonCries latest={cries.latest} legacy={cries.legacy} />}
-    </ScrollView>
+    <View style={styles.container}>
+      <ScrollView style={styles.scrollView} showsVerticalScrollIndicator={false}>
+        <PokemonCries latest={cries.latest} legacy={cries.legacy} />
+
+        <PokeTabSectionHeader title={'Gallery'} />
+
+        {imageUris.length > 0 && (
+          <View>
+            <View style={styles.thumbnailContainer}>
+              {imageUris.map((uri, index) => (
+                <Image key={index} source={{ uri }} style={styles.thumbnail} resizeMode="cover" />
+              ))}
+            </View>
+          </View>
+        )}
+      </ScrollView>
+
+      <SmallRoundButton onPress={pickAndUploadImage} iconName="add" />
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    gap: 10
+    flex: 1,
+    position: 'relative'
   },
-  description: {
-    fontSize: 16,
-    marginBottom: 8
+  scrollView: {
+    paddingBottom: 80
   },
-  loaderContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
+  thumbnailContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
   },
-  errorContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1
+  thumbnail: {
+    width: '15%',
+    height: 50,
+    aspectRatio: 1,
+    marginBottom: 10,
+    marginHorizontal: '0.5%',
+    borderRadius: 8
   }
 })
