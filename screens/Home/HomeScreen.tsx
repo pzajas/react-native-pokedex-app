0e4d02f9d7ea7052b68a12164cb8d9a8db8c4aa1
@pzajas <zajas.piotr@gmail.com>
2025-09-03 23:16:40 +0200

    Home Screen
    - Fix the dynamic bottom padding
    - Fix the issue with the input field

--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,13 +1,22 @@
-import { ScrollView, View } from 'react-native';
+import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs';
+import { useState } from 'react';
+import { Platform, ScrollView, View } from 'react-native';
 import { NewsListSection } from './list/NewsListSection';
 import { SearchSection } from './search/SearchSection';
 import { TilesSection } from './tiles/TilesSection';
 
 export const HomeScreen = () => {
+  const tabBarHeight = useBottomTabBarHeight();
+  const bottomInset = Platform.OS === 'ios' ? tabBarHeight : 0;
+  const [query, setQuery] = useState('');
+
   return (
     <View className="flex-1 bg-surfaceSecondary dark:bg-surfaceSecondary-dark">
-      <ScrollView contentContainerClassName="pb-16" showsVerticalScrollIndicator={false}>
-        <SearchSection value="" onChangeText={() => {}} />
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: bottomInset }}
+        showsVerticalScrollIndicator={false}
+      >
+        <SearchSection value={query} onChangeText={setQuery} />
         <TilesSection />
         <NewsListSection />
       </ScrollView>
